9d62135145d3107d28fcfbf43d1211d9297dd6d6
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-05-02 14:33:30 +0200

    some corrections

--- index.php
+++ index.php
@@ -5,18 +5,15 @@ Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito 
 con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 <?php
-
 $name = $_GET["subject"];
 $text = "Ciao questo è il mio primo esercizio in " . $name;
 $altText = str_replace("PHP", "***", $text);
-
 ?>
 
-
 <h1> <?php echo ($text) ?> </h1>
-<p>La strina sopra ha lunghezza <?php echo (strlen($text)) ?></p>
+<p>La stringa sopra ha lunghezza <?php echo (strlen($text)) ?></p>
 
 <hr />
 
 <h1> <?php echo ($altText) ?> </h1>
-<p>La strina sopra ha lunghezza <?php echo (strlen($altText)) ?></p>
+<p>La stringa sopra ha lunghezza <?php echo (strlen($altText)) ?></p>
